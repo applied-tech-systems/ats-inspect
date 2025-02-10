b1a69489faef84e3f0cdd725bed4788a9cd3bb28
Tom Bell <Tom.Bell@ats-global.com>
2025-02-10 08:57:44 -0500

    Add custom control project.

---
+++ DemoCustomControl/DemoControl.cs
@@ -0,0 +1,26 @@
+﻿namespace ATS.Inspect.Demo;
+
+public partial class DemoControl : UserControl
+{
+   #region Constructors
+
+   public DemoControl()
+   {
+      InitializeComponent();
+   }
+
+   #endregion
+
+   #region Event Handlers
+
+   #region Button Events
+
+   private void _btnClickMe_Click(object sender, EventArgs e)
+   {
+      MessageBox.Show("You clicked on the button!");
+   }
+
+   #endregion
+
+   #endregion
+}
